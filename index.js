a7206d7da7ba23ac29b3b829df25e9ca9fd42233
MarvinNox <90721671+MarvinNox@users.noreply.github.com>
2025-04-04 20:26:59 +0000

    Deploying to gh-pages from @ MarvinNox/products-sale@cc5fb86b2e1a21672727f384f191da286c7861a4 🚀

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-import"./assets/styles-DLS-0iiV.js";import{a as _,i as a}from"./assets/vendor-X279WPgE.js";const s={listCategories:document.querySelector(".categories"),listProducts:document.querySelector(".products"),notFound:document.querySelector(".not-found"),loader:document.querySelector(".loader"),modal:document.querySelector(".modal"),productModal:document.querySelector(".modal-product"),closeModalBttn:document.querySelector(".modal__close-btn")};let p={BASE_URL_ALL:"https://dummyjson.com/products?limit=12&skip=12",SEARCH_KEY:"search?q=",CATEGORIES_KEY:"https://dummyjson.com/products/category-list"};function r(t){return _.get(`${t}`)}function g(t){t.unshift("all");const e=t.map(c=>`<li class="categories__item">
-            <button class="categories__btn" type="button">${c}</button>
+import"./assets/styles-B8WhyOyT.js";import{a as f,i as c}from"./assets/vendor-X279WPgE.js";const o={listCategories:document.querySelector(".categories"),listProducts:document.querySelector(".products"),notFound:document.querySelector(".not-found"),loader:document.querySelector(".loader"),modal:document.querySelector(".modal"),productModal:document.querySelector(".modal-product"),loadMoreBtn:document.querySelector(".load-more__btn")};let m={BASE_URL_ALL:"https://dummyjson.com/products?limit=12&skip=12",SEARCH_KEY:"search?q=",CATEGORIES_KEY:"https://dummyjson.com/products/category-list"};function n(t){return f.get(`${t}`)}function y(t){t.unshift("all");const s=t.map(a=>`<li class="categories__item">
+            <button class="categories__btn" type="button">${a}</button>
         </li>
-        `).join("");s.listCategories.innerHTML=e}function u(t){const e=t.map(({id:c,images:o,description:d,title:n,brand:l,category:i,price:m})=>`<li class="products__item" data-id="${c}">
-            <img class="products__image" src="${o[0]}" alt="${d}"/>
-            <p class="products__title">${n}</p>
+        `).join("");o.listCategories.innerHTML=s}function u(t){const s=t.map(({id:a,images:e,description:d,title:r,brand:l,category:i,price:g})=>`<li class="products__item" data-id="${a}">
+            <img class="products__image" src="${e[0]}" alt="${d}"/>
+            <p class="products__title">${r}</p>
             <p class="products__brand"><span class="products__brand--bold">Brand: ${l}</span></p>
             <p class="products__category">Category: ${i} </p>
-            <p class="products__price">Price: ${m} $</p>
-        </li>`).join("");s.listProducts.innerHTML=e}function $({images:t,description:e,title:c,tags:o,shippingInformation:d,returnPolicy:n,price:l}){const i=`<img class="modal-product__img" src="${t[0]}" alt="${e}" />
+            <p class="products__price">Price: ${g} $</p>
+        </li>`).join("");o.listProducts.innerHTML=s}function $({images:t,description:s,title:a,tags:e,shippingInformation:d,returnPolicy:r,price:l}){const i=`<img class="modal-product__img" src="${t[0]}" alt="${s}" />
         <div class="modal-product__content">
-        <p class="modal-product__title">${c}</p>
-        <ul class="modal-product__tags">${o.join(", ")}</ul>
-        <p class="modal-product__description">${e}</p>
+        <p class="modal-product__title">${a}</p>
+        <ul class="modal-product__tags">${e.join(", ")}</ul>
+        <p class="modal-product__description">${s}</p>
         <p class="modal-product__shipping-information">Shipping: ${d}</p>
-        <p class="modal-product__return-policy">Return Policy: ${n}</p>
+        <p class="modal-product__return-policy">Return Policy: ${r}</p>
         <p class="modal-product__price">Price: ${l} $</p>
         
         </div>
-    `;s.productModal.innerHTML=i}function f(){s.notFound.classList.add("not-found--visible")}function h(){s.notFound.classList.contains("not-found--visible")&&s.notFound.classList.remove("not-found--visible")}function y(){s.modal.classList.add("modal--is-open")}function L(){s.modal.classList.contains("modal--is-open")&&s.modal.classList.remove("modal--is-open")}function E(t){if(t.target.tagName==="BUTTON"){const e=t.target.textContent,c=`https://dummyjson.com/products/category/${e}`;if(e==="all"){r(p.BASE_URL_ALL).then(o=>u(o.data.products)).catch(o=>a.error({message:`${o.message}`}));return}else r(c).then(o=>{if(o.data.products.length===0){console.log("HEY"),f();return}h(),u(o.data.products)}).catch(o=>a.error({message:`${o.message}`}))}}function S(t){if(t.target.tagName!="UL"){const e=t.target.closest("li").dataset.id;r(`https://dummyjson.com/products/${e}`).then(c=>{$(c.data),y()}).catch(c=>a.error({message:`${c.message}`}))}}let b=1;const C=`https://dummyjson.com/products?limit=12&skip=${(b-1)*12}`;r(p.CATEGORIES_KEY).then(t=>g(t.data)).catch(t=>a.error({message:`${t.message}`}));r(C).then(t=>u(t.data.products)).catch(t=>a.error({message:`${t.message}`}));s.listCategories.addEventListener("click",E);s.listProducts.addEventListener("click",S);s.closeModalBttn.addEventListener("click",L);
+    `;o.productModal.innerHTML=i}function h(){o.notFound.classList.add("not-found--visible")}function L(){o.notFound.classList.contains("not-found--visible")&&o.notFound.classList.remove("not-found--visible")}function E(){o.loader.style.display="none"}function S(){o.modal.classList.add("modal--is-open"),document.addEventListener("keydown",p)}function b(t){(t.target.classList.contains("modal__close-btn")||t.target===t.currentTarget)&&_()}function p(t){t.key==="Escape"&&_()}function _(){o.modal.classList.contains("modal--is-open")&&o.modal.classList.remove("modal--is-open"),document.removeEventListener("keydown",p)}function k(t){if(t.target.tagName==="BUTTON"){const s=t.target.textContent,a=`https://dummyjson.com/products/category/${s}`;if(s==="all"){n(m.BASE_URL_ALL).then(e=>u(e.data.products)).catch(e=>c.error({message:`${e.message}`}));return}else n(a).then(e=>{if(e.data.products.length===0){console.log("HEY"),h();return}L(),u(e.data.products)}).catch(e=>c.error({message:`${e.message}`}))}}function M(t){if(t.target.tagName!="UL"){const s=t.target.closest("li").dataset.id;n(`https://dummyjson.com/products/${s}`).then(a=>{$(a.data),S()}).catch(a=>c.error({message:`${a.message}`}))}}let C=1;const T=`https://dummyjson.com/products?limit=12&skip=${(C-1)*12}`;n(m.CATEGORIES_KEY).then(t=>y(t.data)).catch(t=>c.error({message:`${t.message}`}));n(T).then(t=>u(t.data.products)).catch(t=>c.error({message:`${t.message}`})).finally(()=>E());o.listCategories.addEventListener("click",k);o.listProducts.addEventListener("click",M);o.modal.addEventListener("click",b);
 //# sourceMappingURL=index.js.map
